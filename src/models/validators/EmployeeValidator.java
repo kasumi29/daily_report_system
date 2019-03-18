640e38b6f146bcf5d2c87b52817aeb6508b00b84
Garnet <mellow.garnet@gmail.com>
2019-03-18 21:47:40 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -6,7 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import models.Employee;
-import util.DBUtil;
+import utils.DBUtil;
 
 public class EmployeeValidator {
     public static List<String> validate(Employee e, Boolean code_duplicate_check_flag, Boolean password_check_flag){
